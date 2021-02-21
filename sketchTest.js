2bbde9b93652d10f6b6000c6ce02f4ee227faa37
sariburak <43667154+sariburak@users.noreply.github.com>
2021-02-21 19:10:34 +0300

    Adding a testing tool for maze creation

---
+++ sketchTest.js
@@ -0,0 +1,103 @@
+let rows = 10;
+let cols = 20;
+let height = 600;
+let width = 1200;
+let w;
+let h;
+let grid = new Array(rows);
+
+//A custom button that I created to check if button is hidden
+//I know I dont need a new definiton just to check isHidden but I wanted to see how 
+//inheritance looks b like in JS
+function DisplayButton(text) {
+    let hidden = false;
+
+    this.button = createButton(text);
+    this.size = function() {
+        return this.button.size();
+    };
+    this.position = function(x, y) {
+        this.button.position(x, y);
+    };
+    this.mousePressed = function(f) {
+        this.button.mousePressed(f);
+    };
+    this.hide = function() {
+        this.button.hide();
+        this.hidden = true;
+    }
+
+    this.show = function() {
+        this.button.show();
+        this.hidden = false;
+    }
+
+    this.resize = function(w, h) {
+        this.button.size(w, h);
+    }
+}
+
+function Cell(i, j) {
+    this.i = i;
+    this.j = j;
+    this.x = j * w;
+    this.y = i * h;
+
+    this.buttonEnter = new DisplayButton("enter");
+    this.buttonExit = new DisplayButton("exit");
+
+    let offsetX = w / 2 - this.buttonEnter.size().width / 2;
+    let offsetY = h / 2 - this.buttonEnter.size().height / 2;
+
+    this.buttonEnter.position(this.x + offsetX, this.y + offsetY);
+    // this.buttonEnter.resize(w / 4, h / 4);
+    this.buttonEnter.mousePressed(() => {
+        this.buttonEnter.hide();
+        this.buttonExit.show();
+        // console.log(this);
+    });
+
+    this.buttonExit.position(this.x + offsetX, this.y + offsetY);
+    this.buttonExit.mousePressed(() => {
+        this.buttonExit.hide();
+        this.buttonEnter.show();
+    })
+    this.buttonExit.hide();
+
+    this.show = function() {
+        if (!this.buttonEnter.hidden) {
+            fill(color(0, 0, 255, 100));
+        } else {
+            fill(color(0, 255, 0, 100));
+        }
+        rect(this.x, this.y, w, h);
+    }
+}
+
+function setup() {
+    createCanvas(width, height);
+    w = width / cols;
+    h = height / rows;
+
+    //create grid
+    for (let i = 0; i < rows; i++) {
+        grid[i] = new Array(cols);
+    }
+
+    //create cells
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            grid[i][j] = new Cell(i, j);
+        }
+    }
+}
+
+function draw() {
+    background(51);
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            grid[i][j].show();
+        }
+    }
+}
